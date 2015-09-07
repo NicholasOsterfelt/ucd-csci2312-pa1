1c317bf95e262f0a3e5acccb335b4ba2512de3d5
NicholasOsterfelt <nicholas.osterfelt@ucdenver.edu>
2015-09-07 15:07:50 -0600

    Updated for completion
    Finished pa1.cpp

--- pa1.cpp
+++ pa1.cpp
@@ -1,11 +1,29 @@
 //
-// Created by Ivo Georgiev on 8/25/15.
+// Created by Nick Osterfelt on 9/5/15.
 //
-
+#include <cmath>
+#include <iostream>
 #include "Point.h"
+#include <iomanip>
+using namespace std;
 
+double computeArea(Point &a, Point &b, Point &c);
 int main(void) {
-    Point p;
+
+
+    Point a(0,2,4), b(0,2,4), c(1,3,2);        //declares points a,b, and c
+
+    cout << fixed << setprecision(2) << "The area of the triangle is: " << computeArea(a, b, c);
 
     return 0;
 }
+
+double computeArea(Point &a, Point &b, Point &c){
+    double ab = a.distanceTo(b);            //uses distanceTo function of point to calculate distances of triangle
+    double bc = b.distanceTo(c);
+    double ca = c.distanceTo(a);
+    return .25*(sqrt((ab+bc+ca)*(bc+ca-ab)*(ab-bc+ca)*(ab+bc-ca))); //uses Heron's Formula to calculate area of triangle
+
+}
+// Computes the area of the triangle created by 3 points.
+
