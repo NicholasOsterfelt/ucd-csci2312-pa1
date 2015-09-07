96fb5f081c12682ec8d78b632c2a31072c7d2018
NicholasOsterfelt <nicholas.osterfelt@ucdenver.edu>
2015-09-07 15:41:27 -0600

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -3,51 +3,54 @@
 // Default constructor
 // Initializes the point to (0.0, 0.0)
 Point::Point() {
-  x = 0.0;
-  y = 0.0;
-  z = 0.0;
+    x = 0.0;
+    y = 0.0;
+    z = 0.0;
 }
 
 // Constructor
 // Initializes the point to (initX, initY)
 Point::Point(double initX, double initY, double initZ) {
-  x = initX;
-  y = initY;
-  z = initZ;
+    x = initX;
+    y = initY;
+    z = initZ;
 }
 
 // Destructor
 // No dynamic allocation, so nothing to do; if omitted, generated automatically
 Point::~Point() {
-  // no-op
+    // no-op
 }
 
 // Mutator methods
 // Change the values of private member variables
 
 void Point::setX(double newX) {
-  x = newX;
+    x = newX;
 }
 
 void Point::setY(double newY) {
-  y = newY;
+    y = newY;
+}
+void Point:: setZ(double newZ) {
+    z = newZ;
 }
 
 // Accessors
 // Return the current values of private member variables
 const double Point::getX() {
-  return x;
+    return x;
 }
 
 const double Point::getY() {
-  return y;
+    return y;
 }
 const double Point::getZ() {
-  return z;
+    return z;
 }
 // Member functions
 // Calculates the distance between two points
 const double Point::distanceTo(Point & point){
-  return sqrt(pow((point.getX()-x), 2) + pow((point.getY()-y),2) + pow((point.getZ()-z),2));
+    return sqrt(pow((point.getX()-x), 2) + pow((point.getY()-y),2) + pow((point.getZ()-z),2));
 
 }
